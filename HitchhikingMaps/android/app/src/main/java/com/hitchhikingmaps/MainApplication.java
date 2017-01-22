50c0271a7513a635d2242d3de1b576a79a225873
Amaury Martiny <amaury.martiny@gmail.com>
2017-01-22 22:39:34 +0100

    Update all packages to latest version (incl RN0.40)

--- HitchhikingMaps/android/app/src/main/java/com/hitchhikingmaps/MainApplication.java
+++ HitchhikingMaps/android/app/src/main/java/com/hitchhikingmaps/MainApplication.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,14 +24,19 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }

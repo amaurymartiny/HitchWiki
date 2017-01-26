13aafcddbf7bb6e6e0efd3f32806ccbadffa96f8
Amaury Martiny <amaury.martiny@gmail.com>
2017-01-26 11:42:13 +0100

    Add elements in ShopDetails view

--- HitchhikingMaps/src/modules/SpotDetails/SpotDetailsView.js
+++ HitchhikingMaps/src/modules/SpotDetails/SpotDetailsView.js
@@ -1,16 +1,17 @@
-import React, { PropTypes } from 'react';
-import { ScrollView } from 'react-native';
-import { Tile, Heading, Title, Subtitle, Text, ListView, View, RichMedia, Icon } from '@shoutem/ui';
-import StarRating from 'react-native-star-rating';
+import React, { PropTypes } from "react";
+import { Screen, View, ScrollView, Heading, Title, Subtitle, Text, Caption, Divider, ListView, Tile, RichMedia, Image, Row, Icon} from "@shoutem/ui";
+import RNIcon from "react-native-vector-icons/FontAwesome";
+import StarRating from "react-native-star-rating";
+import * as moment from "moment";
 
-import * as Actions from './SpotDetailsState';
+import * as Actions from "./SpotDetailsState";
 
 class SpotDetailsView extends React.Component {
 
   static route = {
     navigationBar: {
       visible: true,
-      title: 'Spot Details',
+      title: "Spot Details",
     },
   }
 
@@ -22,15 +23,22 @@ class SpotDetailsView extends React.Component {
     this.props.dispatch(Actions.fetchSpotDetails(this.props.route.params.id))
   }
 
+  _formatDate(timestamp) {
+    const year = timestamp.substring(0,4);
+    const month = timestamp.substring(4,6);
+    const day = timestamp.substring(6,8);
+    return day + '.' + month + '.' + year;
+  }
+
   render() {
     return (
-      <ScrollView>
-        <Tile styleName="xl-gutter">
+      <ScrollView styleName="md-gutter-left">
 
+        <Tile styleName="xl-gutter">
           <Heading>{this.props.Cities.length ? this.props.Cities[this.props.Cities.length - 1] : this.props.title}</Heading>
           <Subtitle>
             {this.props.Cities.slice(0, -1).map((city) => {
-              return city + ', ';
+              return city + ", ";
             })}
             {this.props.Country.length && this.props.Country[0]}
           </Subtitle>
@@ -40,20 +48,34 @@ class SpotDetailsView extends React.Component {
             starSize={25}
             selectedStar={() => {}}
           />
-          <Text styleName="xl-gutter">{this.props.rating_count} <Icon name='ic_user_profile' /></Text>
+          <View styleName="horizontal space-between">
+            <Text><RNIcon name="thumbs-o-up" size={20} /> {this.props.rating_count}</Text>
+            <Text><RNIcon name="comments-o" size={20} /> {this.props.comment_count}</Text>
+            <Text><RNIcon name="hourglass-half" size={20} /> {this.props.waiting_time_average}</Text>
+          </View>
+        </Tile>
 
-          <Title styleName="md-gutter-top">Description</Title>
-          <Text>HEdslfslkdafjdl dsafjklds fjlkdsfj ldksjf lkdsjf lksdjf alkds jfldksaj flksdfj lkdasj flkadsjf.</Text>
+        <Divider styleName="section-header" />
 
-          <Title styleName="md-gutter-top">Comments</Title>
-          <ListView
-            data={this.props.comments}
-            renderRow={comment => {
-              return <Text>{comment.commenttext}</Text>
-            }}
-          />
+        <Tile styleName="md-gutter">
+          <Title><RNIcon name="pencil-square" size={18} /> Description</Title>
+          <Text styleName="md-gutter-vertical">{this.props.Description || "No description available."}</Text>
 
-
+          {this.props.comments.map((comment, index) => {
+            return(
+              <Row key={comment.comment_id}>
+                <Icon name="comment" styleName="top" />
+                <View styleName="vertical">
+                  <View styleName="horizontal space-between">
+                    <Subtitle>{comment.user_name || "Anonymous User"}</Subtitle>
+                    <Caption>{this._formatDate(comment.timestamp)}</Caption>
+                  </View>
+                  <Text styleName="multiline">{comment.commenttext}</Text>
+                </View>
+                <Divider styleName="line" />
+              </Row>
+            )
+          })}
         </Tile>
       </ScrollView>
     );
